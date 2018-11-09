cdcd78c31e2c3370f6fdc4761bd11f387996431b
chawthinzar <44886989+chawthinzar@users.noreply.github.com>
2018-11-09 12:34:19 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+// THE SPACE FOR THE LARGE IMAGE
+let current = document.querySelector('#current');
+
+//WITHOUT LOOP (THE LONG WAY)
+const img1=document.getElementByID('img1');
+const img2=document.getElementByID('img2');
+const img3=document.getElementByID('img3');
+const img4=document.getElementByID('img4');
+const img5=document.getElementByID('img5');
+const img6=document.getElementByID('img6');
+const img7=document.getElementByID('img7');
+const img8=document.getElementByID('img8');
+
+// img1.addEventListener('click',function(event){
+//   current.src=event.target.src;
+// });
+// img2.addEventListener('click',function(event){
+//   current.src=event.target.src;
+// });
+// img3.addEventListener('click',function(event){
+//   current.src=event.target.src;
+  
+// });
+// img3.addEventListener('click',function(event){
+//   current.src=event.target.src;
+  
+// });
+// img4.addEventListener('click',function(event){
+//   current.src=event.target.src;
+  
+// });
+// img5.addEventListener('click',function(event){
+//   current.src=event.target.src;
+  
+// });
+// img6.addEventListener('click',function(event){
+//   current.src=event.target.src;
+  
+// });
+// img7.addEventListener('click',function(event){
+//   current.src=event.target.src;
+  
+// });
+// const allImgs=[img1,img2,img3,img4,img5,img6,img7,img8];
+
+// allImgs[0].addEventListener('click',function(event){
+//   current.src=event.target.src;
+// });
+// allImgs[1].addEventListener('click',function(event){
+//   current.src=event.target.src;
+// });
+// allImgs[2].addEventListener('click',function(event){
+//   current.src=event.target.src;
+// });
+// allImgs[3].addEventListener('click',function(event){
+//   current.src=event.target.src;
+// });
+// allImgs[4].addEventListener('click',function(event){
+//   current.src=event.target.src;
+// });
+// allImgs[5].addEventListener('click',function(event){
+//   current.src=event.target.src;
+// });
+// allImgs[6].addEventListener('click',function(event){
+//   current.src=event.target.src;
+// });
+// allImgs[7].addEventListener('click',function(event){
+//   current.src=event.target.src;
+// });
+const imgs =document.querySelectAll('.imgasing');
+const placeName=document.getElementById('placeName');
+
+for(let counter=0;counter<=7;counter=counter+1){
+  img[]
+}
